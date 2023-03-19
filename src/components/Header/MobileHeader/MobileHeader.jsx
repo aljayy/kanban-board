9742678d5847a79593504b528b8ccf5bbc46ea50
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-18 17:51:32 -0700

    feat: add functionality to rotate chevron icon

--- src/components/Header/MobileHeader/MobileHeader.jsx
+++ src/components/Header/MobileHeader/MobileHeader.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./MobileHeader.module.scss";
 import cross from "../../../assets/icon-add-task-mobile.svg";
 import ellipsis from "../../../assets/icon-vertical-ellipsis.svg";
@@ -6,6 +6,12 @@ import logo from "../../../assets/logo-mobile.svg";
 import path from "../../../assets/icon-chevron-down.svg";
 
 function MobileHeader() {
+  const [changeRotation, setChangeRotation] = useState(false);
+
+  function viewAllBoards() {
+    setChangeRotation((prev) => !prev);
+  }
+
   return (
     <header className={classes}>
       <div className={classes["board-title"]}>
@@ -14,8 +20,15 @@ function MobileHeader() {
           alt={"Mobile logo"}
           className={classes["mobile-logo"]}
         />
-        <h2 className={classes["mobile-board-title"]}>Platform Launch</h2>
-        <img src={path} alt={"Chevron icon"} className={classes.path} />
+        <h2 className={classes["mobile-board-title"]} onClick={viewAllBoards}>
+          Platform Launch
+        </h2>
+        <img
+          src={path}
+          alt={"Chevron icon"}
+          className={`${classes.path} ${changeRotation ? classes.rotate : ""}`}
+          onClick={viewAllBoards}
+        />
       </div>
       <div className={classes["board-actions"]}>
         <button className={classes["add-task"]} disabled={true}>
