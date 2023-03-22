767e4801fd441a3f4bc5e1351014e4f8ccfa8acd
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-22 11:37:11 -0700

    feat: add App.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import classes from "./App.module.scss";
+import Header from "./components/Header/Header";
+
 function App() {
-  return <div>Blank page</div>;
+  return (
+    <div className={classes}>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
