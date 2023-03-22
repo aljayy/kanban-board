e7d0b3816237f034f5dd7bb9c834e76993075bc0
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-22 11:40:07 -0700

    feat: add functionality for showing and hiding Sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,30 +4,36 @@ import lightlogo from "../../assets/logo-light.svg";
 import hide from "../../assets/icon-hide-sidebar.svg";
 import AllBoards from "../Header/AllBoards/AllBoards";
 import ThemeToggler from "../Header/ThemeToggler/ThemeToggler";
+import ShowSidebar from "./ShowSidebar/ShowSidebar";
 
-function Sidebar() {
+function Sidebar({ showSidebar, toggleSidebar }) {
+  const positionTransition = showSidebar ? classes.show : "";
+
   return (
-    <div className={classes.sidebar}>
-      <div>
-        <div className={classes["sb-logo-container"]}>
-          <img src={lightlogo} alt="Kanban Logo" />
+    <>
+      <div className={`${classes.sidebar} ${positionTransition}`}>
+        <div>
+          <div className={classes["sb-logo-container"]}>
+            <img src={lightlogo} alt="Kanban Logo" />
+          </div>
+          <div className={classes["sb-all-boards"]}>
+            <AllBoards />
+          </div>
         </div>
-        <div className={classes["sb-all-boards"]}>
-          <AllBoards />
+        <div>
+          <div className={classes["theme-toggle"]}>
+            <ThemeToggler />
+          </div>
+          <li className={classes["hide-sb"]}>
+            <button onClick={toggleSidebar}>
+              <img src={hide} alt="Hide Board Icon" />
+              <p>Hide Sidebar</p>
+            </button>
+          </li>
         </div>
       </div>
-      <div>
-        <div className={classes["theme-toggle"]}>
-          <ThemeToggler />
-        </div>
-        <li className={classes["hide-sb"]}>
-          <button>
-            <img src={hide} alt="Hide Board Icon" />
-            <p>Hide Sidebar</p>
-          </button>
-        </li>
-      </div>
-    </div>
+      <ShowSidebar toggleSidebar={toggleSidebar} />
+    </>
   );
 }
 
