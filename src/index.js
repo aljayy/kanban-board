a2028ede911bf5045caed4055f93e2a179aa5185
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-23 11:36:36 -0700

    feat: add theme context provider for global consumption

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeCtxProvider } from "./context/themectx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeCtxProvider>
+      <App />
+    </ThemeCtxProvider>
   </React.StrictMode>
 );
