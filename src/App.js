dc1ae01c28c8d9f9ccb5b667c341b0bc1e276ee3
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-23 11:24:40 -0700

    style: add theme classes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { useContext } from "react";
 import classes from "./App.module.scss";
 import Header from "./components/Header/Header";
+import ThemeCtx from "./context/themectx";
 
 function App() {
+  const { theme } = useContext(ThemeCtx);
+
   return (
-    <div className={classes}>
+    <div className={`${classes} ${classes[theme]}`}>
       <Header />
     </div>
   );
