32b270dbc715a5e464dd5741dce911f83b31b78e
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-23 11:29:24 -0700

    style: add theme classes

--- src/components/Header/MobileBoardConfig/MobileMenuModal.jsx
+++ src/components/Header/MobileBoardConfig/MobileMenuModal.jsx
@@ -1,19 +1,21 @@
-import React from "react";
-import AllBoards from "../AllBoards/AllBoards";
-import ThemeToggler from "../ThemeToggler/ThemeToggler";
+import React, { useContext } from "react";
 import classes from "./MobileMenuModal.module.scss";
+import AllBoards from "../AllBoards/AllBoards";
+import ThemeCtx from "../../../context/themectx";
+import ThemeToggler from "../ThemeToggler/ThemeToggler";
 
 function MobileModal({ viewMenu, toggleMenu }) {
   const opacityTransition = viewMenu
     ? classes["show-menu"]
     : classes["hide-menu"];
+  const { theme } = useContext(ThemeCtx);
 
   return (
     <div
       className={`${classes.overlay} ${opacityTransition}`}
       onClick={toggleMenu}
     >
-      <menu onClick={(e) => e.stopPropagation()}>
+      <menu onClick={(e) => e.stopPropagation()} className={classes[theme]}>
         <div className={classes["mobile-all-boards"]}>
           <AllBoards />
         </div>
