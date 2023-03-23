76de7df8f5497507caf5487d0716d1cf32739b65
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-23 11:26:38 -0700

    style: add theme classes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import classes from "./Header.module.scss";
 import MobileBoardConfig from "./MobileBoardConfig/MobileBoardConfig";
 import BoardConfig from "./BoardConfig/BoardConfig";
+import ThemeCtx from "../../context/themectx";
 
 function Header() {
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  const { theme } = useContext(ThemeCtx);
 
   useEffect(() => {
     const viewportSize = () => {
@@ -21,7 +23,9 @@ function Header() {
   const deviceClasses = isMobile ? classes["mobile-configs"] : classes.configs;
   const configs = isMobile ? <MobileBoardConfig /> : <BoardConfig />;
 
-  return <header className={deviceClasses}>{configs}</header>;
+  return (
+    <header className={`${deviceClasses} ${classes[theme]}`}>{configs}</header>
+  );
 }
 
 export default Header;
