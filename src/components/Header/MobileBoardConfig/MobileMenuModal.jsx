fe2e9da8fca523ad106185c69a3281d012e027bd
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-22 18:01:53 -0700

    style: add transition class

--- src/components/Header/MobileBoardConfig/MobileMenuModal.jsx
+++ src/components/Header/MobileBoardConfig/MobileMenuModal.jsx
@@ -3,11 +3,16 @@ import AllBoards from "../AllBoards/AllBoards";
 import ThemeToggler from "../ThemeToggler/ThemeToggler";
 import classes from "./MobileMenuModal.module.scss";
 
-function MobileModal({ viewMenu }) {
-  if (!viewMenu) return;
+function MobileModal({ viewMenu, toggleMenu }) {
+  const opacityTransition = viewMenu
+    ? classes["show-menu"]
+    : classes["hide-menu"];
 
   return (
-    <div className={classes.overlay}>
+    <div
+      className={`${classes.overlay} ${opacityTransition}`}
+      onClick={toggleMenu}
+    >
       <menu>
         <div className={classes["mobile-all-boards"]}>
           <AllBoards />
