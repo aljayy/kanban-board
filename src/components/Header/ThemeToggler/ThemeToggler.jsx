ba2985d92729dd97100b7091651fdd42f241c3e6
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-22 17:15:53 -0700

    feat: consume themectx.js context state

--- src/components/Header/ThemeToggler/ThemeToggler.jsx
+++ src/components/Header/ThemeToggler/ThemeToggler.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./ThemeToggler.module.scss";
 import darkIcon from "../../../assets/icon-dark-theme.svg";
 import lightIcon from "../../../assets/icon-light-theme.svg";
+import ThemeCtx from "../../../context/themectx";
 
 function ThemeToggler() {
+  const { isLight, toggleTheme } = useContext(ThemeCtx);
+
+  const toggleTransition = isLight ? "" : classes.transform;
+
   return (
     <div className={classes["theme-toggler"]}>
       <img src={lightIcon} alt="Light Theme Icon" />
-      <button>
-        <div />
+      <button onClick={toggleTheme}>
+        <div className={toggleTransition} />
       </button>
       <img src={darkIcon} alt="Dark Theme Icon" />
     </div>
