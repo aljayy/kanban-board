f6e701813c7be4a9406402cd0d19ff4e096d284e
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-28 14:42:23 -0700

    feat: add BoardCtxProvider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BoardCtxProvider } from "./context/boardctx";
 import { ThemeCtxProvider } from "./context/themectx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeCtxProvider>
-      <App />
+      <BoardCtxProvider>
+        <App />
+      </BoardCtxProvider>
     </ThemeCtxProvider>
   </React.StrictMode>
 );
