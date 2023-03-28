d9bfa5df3e49a911377c5d850e9f14302ab889d4
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-28 16:27:55 -0700

    feat: add toggle function from boardctx.js context file

--- src/components/Board/EmptyBoard/EmptyBoard.jsx
+++ src/components/Board/EmptyBoard/EmptyBoard.jsx
@@ -3,13 +3,13 @@ import classes from "./EmptyBoard.module.scss";
 import BoardCtx from "../../../context/boardctx";
 
 function EmptyBoard() {
-  const { showSidebar } = useContext(BoardCtx);
+  const { showSidebar, toggleEditBoardModal } = useContext(BoardCtx);
   const transition = showSidebar ? classes["left-transition"] : "";
 
   return (
     <div className={`${classes["empty-board-wrapper"]} ${transition}`}>
       <h2>This board is empty. Create a new column to get started.</h2>
-      <button>
+      <button onClick={toggleEditBoardModal}>
         <p>+ Add New Column</p>
       </button>
     </div>
