46fc4ff20d4382b388b69574a0bada6a86ace5dc
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-29 14:53:33 -0700

    feat: add EditBoard component

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import EmptyBoard from "./EmptyBoard/EmptyBoard";
+import EditBoard from "../Actions/EditBoard/EditBoard";
 
 function Board() {
   return (
     <>
       <EmptyBoard />
+      <EditBoard />
     </>
   );
 }
