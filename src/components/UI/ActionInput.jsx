5c384601870005f7b42ed33cc805f586f0e6b8b7
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-29 15:01:46 -0700

    style: add theme mode classes

--- src/components/UI/ActionInput.jsx
+++ src/components/UI/ActionInput.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./ActionInput.module.scss";
+import ThemeCtx from "../../context/themectx";
 
 function ActionInput({ input }) {
+  const { theme } = useContext(ThemeCtx);
+  const labelTheme = classes[theme + "-label"];
+  const inputTheme = classes[theme + "-input"];
+
   return (
     <>
-      <label className={classes["action-input-label"]} for={input.id}>
+      <label
+        className={`${classes["action-input-label"]} ${labelTheme}`}
+        for={input.id}
+      >
         {input.label}
       </label>
       <input
-        className={classes["action-input"]}
+        className={`${classes["action-input"]} ${inputTheme}`}
         placeholder={input.placeholder}
         type={input.type}
         id={input.id}
