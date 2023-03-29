366c63832a69469820baf045dfca2c9aafba744c
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-29 16:03:47 -0700

    feat: add state for handling the EditBoard component

--- src/context/boardctx.js
+++ src/context/boardctx.js
@@ -1,19 +1,33 @@
 import React, { useState } from "react";
 
 const BoardCtx = React.createContext({
+  showEditBoard: Boolean,
   showSidebar: Boolean,
+  toggleEditBoardModal: () => {},
   toggleSidebar: () => {},
 });
 
 export const BoardCtxProvider = ({ children }) => {
   const [showSidebar, setShowSidebar] = useState(false);
+  const [showEditBoard, setShowEditBoard] = useState(false);
 
   function toggleSidebar() {
     setShowSidebar((prev) => !prev);
   }
 
+  function toggleEditBoardModal() {
+    setShowEditBoard((prev) => !prev);
+  }
+
   return (
-    <BoardCtx.Provider value={{ showSidebar, toggleSidebar }}>
+    <BoardCtx.Provider
+      value={{
+        showEditBoard,
+        showSidebar,
+        toggleEditBoardModal,
+        toggleSidebar,
+      }}
+    >
       {children}
     </BoardCtx.Provider>
   );
