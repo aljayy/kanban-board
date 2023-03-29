4af9734c4209fb7ffbde5a9335c51a6247fdeec4
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-29 15:02:41 -0700

    style: add theme mode classes

--- src/components/UI/ActionTagInput.jsx
+++ src/components/UI/ActionTagInput.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./ActionTagInput.module.scss";
 import deleteIcon from "../../assets/icon-cross.svg";
 import ButtonSecondary from "./Buttons/ButtonSecondary";
+import ThemeCtx from "../../context/themectx";
 
 function ActionTagInput({ input }) {
+  const { theme } = useContext(ThemeCtx);
+  const labelTheme = classes[theme + "-tag-label"];
+  const wrapperTheme = classes[theme + "-tag-wrapper"];
+
   return (
     <>
-      <label className={classes["action-tag-label"]}>{input.label}</label>
+      <label className={`${classes["action-tag-label"]} ${labelTheme}`}>
+        {input.label}
+      </label>
       {input.columns.map((column) => {
         return (
-          <div className={classes["action-tag-wrapper"]}>
+          <div className={`${classes["action-tag-wrapper"]} ${wrapperTheme}`}>
             <input type={input.type} defaultValue={column.title} />
             <img src={deleteIcon} alt="Delete Icon" />
           </div>
