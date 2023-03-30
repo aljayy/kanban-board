860ec1cafc216f39e70401ec0117c0a2888753e3
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-30 14:51:30 -0700

    feat: show current active board

--- src/components/Header/AllBoards/AllBoards.jsx
+++ src/components/Header/AllBoards/AllBoards.jsx
@@ -1,23 +1,27 @@
-import React from "react";
-import data from "../../../data.json";
+import React, { useContext } from "react";
 import inactiveBoardIcon from "../../../assets/icon-board.svg";
 import activeBoardIcon from "../../../assets/icon-active-board.svg";
 import newBoardIcon from "../../../assets/icon-new-board.svg";
 import classes from "./AllBoards.module.scss";
+import BoardCtx from "../../../context/boardctx";
 
 function AllBoards() {
-  const localData = data.boards;
+  const { activeBoard, boards } = useContext(BoardCtx);
+
   return (
     <>
-      <h3 className={classes}>{`All Boards (${localData.length})`}</h3>
-      {localData.map((board, index) => {
-        let activeBoard =
-          index === 0 ? classes["active-board"] : classes["non-active-board"];
+      <h3 className={classes}>{`All Boards (${boards.length})`}</h3>
+      {boards.map((board) => {
+        let active =
+          activeBoard.id === board.id
+            ? classes["active-board"]
+            : classes["non-active-board"];
 
-        let icon = index === 0 ? activeBoardIcon : inactiveBoardIcon;
+        let icon =
+          activeBoard.id === board.id ? activeBoardIcon : inactiveBoardIcon;
 
         return (
-          <li key={index} className={activeBoard}>
+          <li key={board.id} className={active}>
             <button>
               <img src={icon} alt="Board Icon" />
               <p>{board.name}</p>
