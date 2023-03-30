53fb7ed6df6faae78e234aca4b6236961c00695b
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-30 12:59:50 -0700

    feat: add uuids to respective objects

--- src/context/boardctx.js
+++ src/context/boardctx.js
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { v4 as uuidv5 } from "uuid";
+import data from "../data.json";
 
 const BoardCtx = React.createContext({
+  boards: [],
   showEditBoard: Boolean,
   showSidebar: Boolean,
   toggleEditBoardModal: () => {},
@@ -8,8 +11,26 @@ const BoardCtx = React.createContext({
 });
 
 export const BoardCtxProvider = ({ children }) => {
+  const [boards, setBoards] = useState([]);
   const [showSidebar, setShowSidebar] = useState(false);
   const [showEditBoard, setShowEditBoard] = useState(false);
+
+  useEffect(() => {
+    setBoards(
+      data.boards.map((board) => ({
+        ...board,
+        id: uuidv5(),
+        columns: board.columns.map((column) => ({
+          ...column,
+          id: uuidv5(),
+          tasks: column.tasks.map((task) => ({
+            ...task,
+            id: uuidv5(),
+          })),
+        })),
+      }))
+    );
+  }, []);
 
   function toggleSidebar() {
     setShowSidebar((prev) => !prev);
@@ -22,6 +43,7 @@ export const BoardCtxProvider = ({ children }) => {
   return (
     <BoardCtx.Provider
       value={{
+        boards,
         showEditBoard,
         showSidebar,
         toggleEditBoardModal,
