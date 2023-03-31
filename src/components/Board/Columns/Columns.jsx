bad22b2b65fa90a39e3f52e24ec44eb973be5582
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-03-31 13:35:44 -0700

    style: add tablet media query

--- src/components/Board/Columns/Columns.jsx
+++ src/components/Board/Columns/Columns.jsx
@@ -4,10 +4,12 @@ import BoardCtx from "../../../context/boardctx";
 import Column from "../Column/Column";
 
 function Columns() {
-  const { activeBoard } = useContext(BoardCtx);
+  const { activeBoard, showSidebar } = useContext(BoardCtx);
+
+  const transitionClass = showSidebar ? classes["transition"] : "";
 
   return (
-    <div className={classes["columns-wrapper"]}>
+    <div className={`${classes["columns-wrapper"]} ${transitionClass}`}>
       {activeBoard.columns &&
         activeBoard.columns.map((column) => {
           return <Column column={column} key={column.id} />;
