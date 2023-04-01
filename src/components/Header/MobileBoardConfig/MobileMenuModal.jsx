50e318631c300720dcff4b53134306dc4d34882d
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-04-01 09:46:11 -0700

    feat: add OverlayPortal component and render MobileMenuModal through portal

--- src/components/Header/MobileBoardConfig/MobileMenuModal.jsx
+++ src/components/Header/MobileBoardConfig/MobileMenuModal.jsx
@@ -1,29 +1,38 @@
 import React, { useContext } from "react";
+import ReactDOM from "react-dom";
 import classes from "./MobileMenuModal.module.scss";
 import AllBoards from "../AllBoards/AllBoards";
+import BoardCtx from "../../../context/boardctx";
 import ThemeCtx from "../../../context/themectx";
 import ThemeToggler from "../ThemeToggler/ThemeToggler";
+import OverlayPortal from "../../UI/OverlayPortal/OverlayPortal";
 
-function MobileModal({ viewMenu, toggleMenu }) {
-  const opacityTransition = viewMenu
+function MobileModal() {
+  const { theme } = useContext(ThemeCtx);
+  const { viewMobileMenu, toggleMobileMenu } = useContext(BoardCtx);
+  const opacityTransition = viewMobileMenu
     ? classes["show-menu"]
     : classes["hide-menu"];
-  const { theme } = useContext(ThemeCtx);
 
   return (
-    <div
-      className={`${classes.overlay} ${opacityTransition}`}
-      onClick={toggleMenu}
-    >
-      <menu onClick={(e) => e.stopPropagation()} className={classes[theme]}>
-        <div className={classes["mobile-all-boards"]}>
-          <AllBoards />
-        </div>
-        <div className={classes["theme-toggler-wrapper"]}>
-          <ThemeToggler />
-        </div>
-      </menu>
-    </div>
+    <>
+      {ReactDOM.createPortal(
+        <OverlayPortal
+          toggleMobileMenu={toggleMobileMenu}
+          viewMobileMenu={viewMobileMenu}
+        >
+          <menu onClick={(e) => e.stopPropagation()} className={classes[theme]}>
+            <div className={classes["mobile-all-boards"]}>
+              <AllBoards />
+            </div>
+            <div className={classes["theme-toggler-wrapper"]}>
+              <ThemeToggler />
+            </div>
+          </menu>
+        </OverlayPortal>,
+        document.getElementById("overlay-root")
+      )}
+    </>
   );
 }
 
