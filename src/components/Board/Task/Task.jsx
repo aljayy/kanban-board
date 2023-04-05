580892ef1ff9fda323934dfff9036c5be491a8f2
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-04-05 12:08:07 -0700

    feat: add viewTaskDetails func

--- src/components/Board/Task/Task.jsx
+++ src/components/Board/Task/Task.jsx
@@ -1,12 +1,22 @@
 import React, { useContext } from "react";
 import classes from "./Task.module.scss";
+import BoardCtx from "../../../context/boardctx";
 import ThemeCtx from "../../../context/themectx";
 
-function Task({ task }) {
+function Task({ task, componentIds }) {
+  const { toggleTaskDetailsModal, setIds } = useContext(BoardCtx);
   const { theme } = useContext(ThemeCtx);
 
+  function viewTaskDetails() {
+    setIds(componentIds);
+    toggleTaskDetailsModal();
+  }
+
   return (
-    <div className={`${classes.task} ${classes[theme]}`}>
+    <div
+      className={`${classes.task} ${classes[theme]}`}
+      onClick={() => viewTaskDetails(task)}
+    >
       <p className={classes["task-title"]}>{task.title}</p>
       <p
         className={classes["subtasks"]}
