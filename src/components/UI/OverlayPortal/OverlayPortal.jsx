3e1e60b7669d62121495ec4ee58548b358c827fd
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-04-06 09:25:45 -0700

    style: position component programmatically based on scroll value

--- src/components/UI/OverlayPortal/OverlayPortal.jsx
+++ src/components/UI/OverlayPortal/OverlayPortal.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
 function OverlayPortal({ classes, onClick, children }) {
+  const [position, setPosition] = useState({ top: 0, left: 0 });
+
+  useEffect(() => {
+    function handleScroll() {
+      setPosition({ top: window.scrollY, left: window.scrollX });
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
       {ReactDOM.createPortal(
-        <div className={classes} onClick={onClick}>
+        <div
+          className={classes}
+          onClick={onClick}
+          style={{ top: `${position.top}px`, left: `${position.left}px` }}
+        >
           {children}
         </div>,
         document.getElementById("overlay-root")
