db89dc3393f9ba4f2c0c01b7c9f7ac7fecf0e43c
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-04-06 15:41:58 -0700

    feat: display current column name as status

--- src/components/Actions/ChangeTaskStatus/ChangeTaskStatus.jsx
+++ src/components/Actions/ChangeTaskStatus/ChangeTaskStatus.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import classes from "./ChangeTaskStatus.module.scss";
 import chevron from "../../../assets/icon-chevron-down.svg";
+import BoardCtx from "../../../context/boardctx";
 
-function ChangeTaskStatus({ theme }) {
+function ChangeTaskStatus() {
+  const [currentStatus, setCurrentStatus] = useState();
+  const { boards, theme, ids } = useContext(BoardCtx);
+
+  useEffect(() => {
+    let status = boards
+      .find((board) => board.isActive)
+      .columns.filter((column) => column.id === ids.column)[0].name;
+
+    setCurrentStatus(status);
+  }, [boards, ids]);
   return (
     <div className={`${classes.status} ${classes[theme]}`}>
       <p>Current Status</p>
       <div className={classes["current-status"]}>
         <div className={classes.selected}>
-          <p>Doing</p>
+          <p>{currentStatus}</p>
           <img src={chevron} alt="Chevron Icon" />
         </div>
-        <ChangeTaskStatus theme={theme} />
         <div></div>
       </div>
     </div>
