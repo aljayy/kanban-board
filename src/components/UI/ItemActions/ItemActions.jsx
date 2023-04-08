62b8876139b7ad2bd2a5195b9ac1547bd3ffc8ab
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-04-08 11:57:51 -0700

    style: add theme styles

--- src/components/UI/ItemActions/ItemActions.jsx
+++ src/components/UI/ItemActions/ItemActions.jsx
@@ -1,15 +1,17 @@
 import React, { useContext } from "react";
 import classes from "./ItemActions.module.scss";
 import BoardCtx from "../../../context/boardctx";
+import ThemeCtx from "../../../context/themectx";
 
 function ItemActions() {
   const { showItemActions } = useContext(BoardCtx);
+  const { theme } = useContext(ThemeCtx);
 
   if (!showItemActions) return;
 
   return (
     <div
-      className={classes["actions-wrapper"]}
+      className={`${classes["actions-wrapper"]} ${classes[theme]}`}
       onClick={(e) => e.stopPropagation()}
     >
       <p className={classes.edit}>Edit Task</p>
