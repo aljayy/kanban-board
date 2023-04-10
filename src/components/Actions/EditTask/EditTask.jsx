a95a71e95377c01f48b2c3127ca364058cc80970
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-04-10 14:13:00 -0700

    feat: add EditTask component

---
+++ src/components/Actions/EditTask/EditTask.jsx
@@ -0,0 +1,72 @@
+import React, { useContext, useEffect, useState } from "react";
+import classes from "./EditTask.module.scss";
+import OverlayPortal from "../../UI/OverlayPortal/OverlayPortal";
+import ModalWrapper from "../../UI/ModalWrapper/ModalWrapper";
+import ActionTitle from "../../UI/ActionTitle";
+import ActionInput from "../../UI/ActionInput";
+import BoardCtx from "../../../context/boardctx";
+import ChangeTaskStatus from "../../Actions/ChangeTaskStatus/ChangeTaskStatus";
+import ActionTagInput from "../../UI/ActionTagInput";
+import SmallButtonPrimary from "../../UI/Buttons/SmallButtonPrimary";
+
+function EditTask() {
+  const { ids, boards, toggleEditTaskModal } = useContext(BoardCtx);
+  const [task, setTask] = useState({});
+
+  useEffect(() => {
+    let currentTask = boards
+      .find((board) => board.isActive)
+      .columns.find((column) => column.id === ids.column)
+      .tasks.find((task) => task.id === ids.task);
+
+    setTask(currentTask);
+  }, [boards, ids.column, ids.task]);
+
+  if (Object.keys(task).length < 1) return;
+
+  return (
+    <OverlayPortal
+      classes={classes["edit-overlay"]}
+      onClick={toggleEditTaskModal}
+    >
+      <ModalWrapper onClick={(e) => e.stopPropagation()}>
+        <div className={classes["action-title"]}>
+          <ActionTitle title={"Edit Task"} />
+        </div>
+        <div className={classes["task-title"]}>
+          <ActionInput
+            input={{
+              label: "Title",
+              id: task.id,
+              type: "text",
+              value: task.title,
+            }}
+          />
+        </div>
+        <div className={classes["task-desc"]}>
+          <label>Description</label>
+          <textarea
+            placeholder={"Shortly describe your task."}
+            defaultValue={task.description}
+          />
+        </div>
+        <div className={classes["action-tag"]}>
+          <ActionTagInput
+            data={{
+              label: "Subtasks",
+              tags: task.subtasks,
+              type: "text",
+              action: "+Add New Subtask",
+            }}
+          />
+        </div>
+        <ChangeTaskStatus />
+        <div className={classes["action-btn"]}>
+          <SmallButtonPrimary text={"Save Changes"} />
+        </div>
+      </ModalWrapper>
+    </OverlayPortal>
+  );
+}
+
+export default EditTask;
