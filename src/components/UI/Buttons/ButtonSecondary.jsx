ef674459a0360158e369a49427a8bf85bc2b360f
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-04-11 15:07:59 -0700

    feat: add onClick

--- src/components/UI/Buttons/ButtonSecondary.jsx
+++ src/components/UI/Buttons/ButtonSecondary.jsx
@@ -2,11 +2,14 @@ import React, { useContext } from "react";
 import classes from "./ButtonSecondary.module.scss";
 import ThemeCtx from "../../../context/themectx";
 
-function ButtonSecondary({ text }) {
+function ButtonSecondary({ text, onClick }) {
   const { theme } = useContext(ThemeCtx);
 
   return (
-    <button className={`${classes["button-secondary"]} ${classes[theme]}`}>
+    <button
+      className={`${classes["button-secondary"]} ${classes[theme]}`}
+      onClick={onClick}
+    >
       <p>{text}</p>
     </button>
   );
