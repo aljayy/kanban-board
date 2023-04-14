5f53643a3a16935e4b1049f335b19c33ab4f0f86
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-04-14 09:21:28 -0700

    feat: add Modal component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import classes from "./App.module.scss";
 import Board from "./components/Board/Board";
 import Header from "./components/Header/Header";
+import Modal from "./components/UI/Modal/Modal";
 import ThemeCtx from "./context/themectx";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className={classes[theme]}>
       <Header />
+      <Modal />
       <Board />
     </div>
   );
