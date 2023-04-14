92af6639885778df18ca1ef463adddfb93be0f78
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-04-14 12:47:18 -0700

    feat: implement functionality to change board data

--- src/components/Actions/EditBoard/EditBoard.jsx
+++ src/components/Actions/EditBoard/EditBoard.jsx
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import classes from "./EditBoard.module.scss";
 import ActionInput from "../../UI/ActionInput";
 import ActionTagInput from "../../UI/ActionTagInput";
@@ -9,27 +10,73 @@ import SmallButtonPrimary from "../../UI/Buttons/SmallButtonPrimary";
 import ModalWrapper from "../../UI/ModalWrapper/ModalWrapper";
 
 function EditBoard() {
-  const { showEditBoard, toggleEditBoardModal } = useContext(BoardCtx);
+  const { showEditBoard, toggleEditBoardModal, boards, changeBoardDetails } =
+    useContext(BoardCtx);
+  const [board, setBoard] = useState();
+  const [titleError, setTitleError] = useState(false);
 
   const transition = showEditBoard ? "" : classes["hide-edit-board"];
 
-  // LOCAL DATA
-  const title = "Edit Board";
+  useEffect(() => {
+    setBoard(boards.find((board) => board.isActive));
+  }, [boards]);
 
-  const inputData = {
-    id: "board-name",
-    label: "Board Name",
-    placeholder: "e.g. Web Design",
-    type: "text",
-  };
+  if (!board) return;
 
-  const tagInputData = {
-    action: "+ Add New Column",
-    columns: [{ title: "Todo" }, { title: "Doing" }, { title: "Done" }],
-    id: "board-columns",
-    label: "Board Columns",
-    type: "text",
-  };
+  function changeBoardTitle(value) {
+    setBoard((prevBoard) => {
+      return { ...prevBoard, name: value };
+    });
+  }
+
+  function changeColumnTitle(value, index) {
+    setBoard((prevBoard) => {
+      return {
+        ...prevBoard,
+        columns: prevBoard.columns.map((column, i) => {
+          if (index === i) {
+            return { ...column, name: value };
+          } else return { ...column };
+        }),
+      };
+    });
+  }
+
+  function addColumn() {
+    setBoard((prevBoard) => {
+      return {
+        ...prevBoard,
+        columns: prevBoard.columns.concat([
+          { name: "", id: uuidv4(), tasks: [] },
+        ]),
+      };
+    });
+  }
+
+  function saveChanges() {
+    if (board.name.trim() === "") {
+      setTitleError(true);
+      return;
+    } else setTitleError(false);
+
+    let columnError = board.columns.map((column) => {
+      if (column.name.trim() === "") {
+        return { ...column, hasError: true };
+      } else {
+        delete column.hasError;
+        return column;
+      }
+    });
+
+    if (columnError.some((column) => column.hasError)) {
+      setBoard((prevBoard) => {
+        return { ...prevBoard, columns: columnError };
+      });
+      return;
+    }
+
+    changeBoardDetails(board);
+  }
 
   return (
     <OverlayPortal
@@ -37,15 +84,34 @@ function EditBoard() {
       onClick={toggleEditBoardModal}
     >
       <ModalWrapper onClick={(e) => e.stopPropagation()}>
-        <ActionTitle title={title} />
+        <div className={classes["action-title"]}>
+          <ActionTitle title={"Edit Board"} />
+        </div>
         <div className={classes["board-name-wrapper"]}>
-          <ActionInput input={inputData} />
+          <ActionInput
+            input={{
+              label: "Board Name",
+              value: board.name,
+              type: "text",
+              id: board.id,
+              onChange: changeBoardTitle,
+              error: titleError,
+            }}
+          />
         </div>
         <div className={classes["tag-input-wrapper"]}>
-          <ActionTagInput input={tagInputData} />
+          <ActionTagInput
+            data={{
+              label: "Board Columns",
+              tags: board.columns,
+              onChange: changeColumnTitle,
+              action: "+ Add New Column",
+              onAction: addColumn,
+            }}
+          />
         </div>
         <div className={classes.save}>
-          <SmallButtonPrimary text={"Save Changes"} />
+          <SmallButtonPrimary text={"Save Changes"} onClick={saveChanges} />
         </div>
       </ModalWrapper>
     </OverlayPortal>
